d23f73a7efb14f1ee2488a3c2bcb5c2c36db797e
GitSyedUmar <89913472+GitSyedUmar@users.noreply.github.com>
2024-01-15 22:09:52 +0530

    Update Pattern1.java

--- Own Sample Programs/Java_Progs/Pattern1.java
+++ Own Sample Programs/Java_Progs/Pattern1.java
@@ -1,1 +1,29 @@
 
+
+import java.util.Scanner;
+
+public class Pattern1
+{
+	public static void main(String[] args) {
+	    
+	    System.out.print("Enter the Input Range: ");
+	    int n = new Scanner(System.in).nextInt();
+	    
+	    for(int i=1;i<=n;i++)
+	    {
+	        for(int j=i;j<=n-1;j++)
+	        {
+	            System.out.print(" ");
+	        }
+	        for(int k=0;k<i;k++)
+	        {
+	            if(i%2!=0)
+	            System.out.print(i);
+	            else
+	            System.out.print("#");
+	        }
+	        System.out.println();
+	    }
+	    
+	}
+}
