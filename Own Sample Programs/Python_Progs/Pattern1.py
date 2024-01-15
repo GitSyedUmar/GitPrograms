bf72425e4bce1380bf100d3bcd2b92c80d6d0b8a
GitSyedUmar <89913472+GitSyedUmar@users.noreply.github.com>
2024-01-15 22:03:57 +0530

    Update Pattern1.py

--- Own Sample Programs/Python_Progs/Pattern1.py
+++ Own Sample Programs/Python_Progs/Pattern1.py
@@ -1,1 +1,19 @@
+# Printing the pattern
 
+#       1
+#      ##
+#     333
+#    ####
+#   55555
+
+n = 5
+for i in range(0,n):
+    for j in range(i,n-1):
+        print(" ", end="")
+    
+    for k in range(i+1):
+        if (i % 2 == 0):
+            print(i+1,end="")
+        else:
+            print("#", end = "")
+    print()
